524b5acb6c30d12f503704e6adc8c474aa8bb480
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-27 08:55:50 +0200

    capv + bump konfigaadm

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-KONFIGADM_VERSION=v0.3.1
-
-echo "image=$image runtime=$runtime 1=$1 2=$2"
+KONFIGADM_VERSION=v0.3.4
+image=$1
+config=$2
 if ! which konfigadm > /dev/null; then
   wget https://github.com/moshloop/konfigadm/releases/download/$KONFIGADM_VERSION/konfigadm.deb
   dpkg -i konfigadm.deb
-fi
-if [[ "$image" == "" ]]; then
-  image=$1
 fi
 if [[ "$image" == "" ]]; then
   echo "Must specify an image: "
@@ -17,11 +14,14 @@ if [[ "$image" == "" ]]; then
 fi
 konfigadm apply -c setup.yml -v
 
-cmd="konfigadm build-image --image $image -c k8s-docker.yml -v"
+cmd="konfigadm build-image --image $image ${config}.yml --resize +2G -v"
 echo "Building image using: $cmd"
 output_image=$($cmd)
 
 echo "Built $output_image"
 
+extension="${output_image##*.}"
+filename="${output_image%.*}"
+
 mkdir -p images
-mv $output_image images/
+mv $output_image images/${config}_${filename}.${extension}
