7d4389bef294b33ac5e081118901c0f37132dcdb
Moshe Immerman <moshe.immerman@gmail.com>
2019-07-23 18:01:30 +0200

    bump konfigadm to 0.4.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o verbose
-KONFIGADM_VERSION=v0.3.6
+KONFIGADM_VERSION=v0.4.0
 image=$(echo $1 |  tr -d '[:space:]')
 config=$(echo $2 |  tr -d '[:space:]')
 if ! which konfigadm > /dev/null; then
@@ -9,12 +9,12 @@ if ! which konfigadm > /dev/null; then
 fi
 if [[ "$image" == "" ]]; then
   echo "Must specify an image: "
-  konfigadm build-image --list-images
+  konfigadm images list
   exit 1
 fi
 konfigadm apply -c setup.yml -v
 filename="$(basename $image | sed 's/:/_/')"
 extension="${filename##*.}"
-filename="$(echo $config)-${filename%.*}-$(date +%Y%m%d%M%H%M%S).img"
+filename="$(echo $config)-${filename%.*}-$(date +"%V%u-%H%M%S").img"
 mkdir -p images
-konfigadm build-image --image "$image" --resize +2G  --output-filename "$filename" --output-dir images "${config}.yml" -v
+konfigadm images build --image "$image" --resize +2G --output-filename "$filename" --output-dir images "${config}.yml" -v
