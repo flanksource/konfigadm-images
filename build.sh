dd49efdacff6236cbf4c6c08455df048e84bb0c8
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-10-02 19:33:26 +0200

    add cloudbuild.yaml

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -o verbose
+NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
+GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
+GITHUB_USER=${GITHUB_USER##*:}
+if [[ "GITHUB_TOKEN" == "" ]]; then
+  GITHUB_TOKEN=$(cat .gh-token)
+fi
+
+if [[ "$TAG" == "" ]]; then
+  TAG=$(git tag --points-at HEAD )
+fi
 KONFIGADM_VERSION=0.4.0
 image=$(echo $1 |  tr -d '[:space:]')
 config=$(echo $2 |  tr -d '[:space:]')
@@ -12,11 +22,38 @@ if [[ "$image" == "" ]]; then
   konfigadm images list
   exit 1
 fi
-konfigadm apply -c setup.yml -v
+
+if ! which qemu-system-x86; then
+  apt-get install -y qemu-system-x86 kpart python-pexpec python-serial libguestfs-tools
+  gsutil cp gs://moshloop-image-builder/VMware-ovftool-4.3.0-12320924-lin.x86_64.bundle .
+  chmod +x VMware-ovftool-4.3.0-12320924-lin.x86_64.bundle
+  ./VMware-ovftool-4.3.0-12320924-lin.x86_64.bundle --eulas-agreed --required
+fi
+# konfigadm apply -c setup.yml -v
 filename="$(basename $image | sed 's/:/_/')"
 extension="${filename##*.}"
 filename="$(echo $config)-${filename%.*}-$(date +"%V%u-%H%M%S").img"
 mkdir -p images
 konfigadm images build --image "$image" --resize +2G --output-filename "$filename" --output-dir images "${config}.yml" -v
+cd images
 konfigadm images convert --image "$image" --output-dir images
-gsutil cp images/* gs://moshloop-image-builder/
+
+go get github.com/aktau/github-release
+
+echo Creating release
+github-release release \
+    --user $GITHUB_USER \
+    --repo $NAME \
+    --tag $TAG \
+    --pre-release
+
+
+for img in $(ls images); do
+  echo Uploading $img
+  github-release upload \
+      --user $GITHUB_USER \
+      --repo $NAME \
+      --tag $TAG \
+      --name $img \
+      --file $img
+done
---
+++ remote-build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+export REPO=$1
+export GIT_SHA=$2
+export TAG=$3
+
+git clone $repo
+cd $(basename $repo)
+git checkout $GIT_SHA
+sudo ./build.sh
+sudo shutdown -h now
+
+
+#!/bin/bash -xe
+
+# Always delete instance after attempting build
+function cleanup {
+    gcloud compute instances delete ${INSTANCE_NAME} --quiet
+}
+
+# Configurable parameters
+[ -z "$COMMAND" ] && echo "Need to set COMMAND" && exit 1;
+
+USERNAME=${USERNAME:-admin}
+REMOTE_WORKSPACE=${REMOTE_WORKSPACE:-/home/${USERNAME}/workspace/}
+INSTANCE_NAME=${INSTANCE_NAME:-builder-$(cat /proc/sys/kernel/random/uuid)}
+ZONE=${ZONE:-us-central1-f}
+INSTANCE_ARGS=${INSTANCE_ARGS:---preemptible}
+
+gcloud config set compute/zone ${ZONE}
+
+KEYNAME=builder-key
+# TODO Need to be able to detect whether a ssh key was already created
+ssh-keygen -t rsa -N "" -f ${KEYNAME} -C ${USERNAME} || true
+chmod 400 ${KEYNAME}*
+
+cat > ssh-keys <<EOF
+${USERNAME}:$(cat ${KEYNAME}.pub)
+EOF
+
+gcloud compute instances create \
+       ${INSTANCE_ARGS} ${INSTANCE_NAME} \
+       --metadata block-project-ssh-keys=TRUE \
+       --metadata-from-file ssh-keys=ssh-keys
+
+trap cleanup EXIT
+
+gcloud compute scp --compress --recurse \
+       $(pwd) ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE} \
+       --ssh-key-file=${KEYNAME}
+
+gcloud compute ssh --ssh-key-file=${KEYNAME} \
+       ${USERNAME}@${INSTANCE_NAME} -- ${COMMAND}
+
+gcloud compute scp --compress --recurse \
+       ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE}* $(pwd) \
+       --ssh-key-file=${KEYNAME}
