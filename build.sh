ee92e647bcac6906ce7d2563e724a41935e9e467
Moshe Immerman <moshe.immerman@gmail.com>
2020-02-03 09:08:16 +0200

    images for 1.16 and 1.17

--- build.sh
+++ build.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 set -o verbose
 
-# [[ "$NAME" == "" ]]         && NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
-# [[ "$GITHUB_USER" == "" ]]  && GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
-# [[ "$GITHUB_TOKEN" == "" ]] && GITHUB_TOKEN=$(cat .gh-token)
-# [[ "$TAG" == "" ]]          && TAG=$(git tag --points-at HEAD )
+export
+[[ "$GITHUB_TOKEN" == "" ]] && GITHUB_TOKEN=$(cat .gh-token)
+[[ "$TAG" == "" ]] && TAG=$(date "+%Y%m%d%H%M%S")
 
 GITHUB_USER=moshloop
 NAME=konfigadm-images
-
-sleep 10
-
-snap install docker
-
-if (! docker stats --no-stream ); then
-  while (! docker stats --no-stream ); do
-    echo "Waiting for Docker to launch..."
-    sleep 1
-  done
-fi
-
+KONFIGADM_VERSION=v0.5.3
 
 image=$(echo $1 |  tr -d '[:space:]')
 config=$(echo $2 |  tr -d '[:space:]')
+cd workspace
+[[ "$GITHUB_TOKEN" == "" ]] && GITHUB_TOKEN=$(cat .gh-token)
+konfigadm="docker run --rm -u root --privileged -v /dev/kvm:/dev/kvm -v $PWD:$PWD -w $PWD -v /root/.konfigadm:/root/.konfigadm flanksource/konfigadm:$KONFIGADM_VERSION konfigadm "
 
-konfigadm="docker run --rm -u root --privileged -v /dev/kvm:/dev/kvm -v $PWD:$PWD -w $PWD -v /root/.konfigadm:/root/.konfigadm flanksource/konfigadm:v0.5.1-1-ga21ebd7"
+$konfigadm version
 
 if [[ "$image" == "" ]]; then
   echo "Must specify an image: "
@@ -35,31 +26,46 @@ fi
 
 filename="$(basename $image | sed 's/:/_/')"
 extension="${filename##*.}"
-filename="$(echo $config)-${filename%.*}-$(date +"%V%u-%H%M%S").img"
+filename="$(echo $config)-${filename%.*}-$(date "+%Y-%m-%d-%H%M%S")"
+ova=${filename}.ova
+filename=${filename}.img
 mkdir -p images
 $konfigadm images build --image "$image" --resize +2G --output-filename "$filename" --output-dir images "${config}.yml" -v
 
 if ! which gitub-release; then
-  wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
+  wget -nv https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
   tar -xjvf linux-amd64-github-release.tar.bz2 --strip-components=3 -C /usr/bin/
 fi
 
-echo Creating release
-github-release release \
+echo Creating release $GITHUB_USER/$NAME/$TAG
+github-release  -v release \
     --user $GITHUB_USER \
     --repo $NAME \
     --tag $TAG \
+    --security-token $GITHUB_TOKEN \
     --pre-release
 
-cd images
-$konfigadm images convert --image "$image" --output-dir images
+$konfigadm images convert --image images/$filename  -v
 
-for img in $(ls images); do
-  echo Uploading $img
-  github-release upload \
-      --user $GITHUB_USER \
-      --repo $NAME \
-      --tag $TAG \
-      --name $img \
-      --file $img
-done
+# for img in $(ls images); do
+echo Uploading $ova
+github-release -v  upload \
+    --user $GITHUB_USER \
+    --repo $NAME \
+    --tag $TAG \
+    --name $ova \
+    --security-token $GITHUB_TOKEN \
+    --file $ova
+
+tar -czvf ${filename}.tgz images/$filename
+
+echo Uploading img
+github-release -v  upload \
+    --user $GITHUB_USER \
+    --repo $NAME \
+    --tag $TAG \
+    --name ${filename}.tgz \
+    --security-token $GITHUB_TOKEN \
+    --file ${filename}.tgz
+
+# done
--- remote-build.sh
+++ remote-build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 # Always delete instance after attempting build
 function cleanup {
   gcloud compute instances delete ${INSTANCE_NAME} --quiet --delete-disks=all
 }
+
 
 # Configurable parameters
 [ -z "$COMMAND" ] && echo "Need to set COMMAND" && exit 1;
@@ -12,11 +14,9 @@ USERNAME=${USERNAME:-admin}
 REMOTE_WORKSPACE=${REMOTE_WORKSPACE:-/home/${USERNAME}/workspace/}
 INSTANCE_NAME=${INSTANCE_NAME:-builder-$(cat /proc/sys/kernel/random/uuid)}
 ZONE=${ZONE:-us-central1-f}
-INSTANCE_ARGS=${INSTANCE_ARGS:---preemptible}
+NESTED_VIRT_NAME=$(gcloud compute images list --no-standard-images --filter 'family:nested-virt' --format 'value(name)')
+INSTANCE_ARGS="$INSTANCE_ARGS --image $NESTED_VIRT_NAME --preemptible"
 
-[[ "$REVISION_ID" == "" ]] && REVISION_ID=$(git tag --points-at HEAD )
-
-export
 gcloud config set compute/zone ${ZONE}
 
 KEYNAME=builder-key
@@ -30,10 +30,28 @@ EOF
 
 gcloud compute instances create \
        ${INSTANCE_ARGS} ${INSTANCE_NAME} \
+       --machine-type=n2-standard-2 \
+       --boot-disk-size=50 \
        --metadata block-project-ssh-keys=TRUE \
        --metadata-from-file ssh-keys=ssh-keys
 
+function wait_for_ssh {
+    echo "Attempting to SSH to Builder machine..."
+    attempt=1
+    while (( $attempt <= 5 )); do
+      gcloud compute ssh  --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --ssh-flag="-Nf" && EC=$?|| EC=$? && true
+      case ${EC} in
+          (0) echo "Success after ${attempt} try"; break ;;
+          (*) echo "${attempt} of 5 failed attempts, Builder SSH Machine not ready yet, waiting 2 seconds..." ;;
+      esac
+      sleep 2s
+      ((attempt+=1))
+    done
+}
+
 trap cleanup EXIT
+
+wait_for_ssh
 
 echo $GITHUB_TOKEN > .gh-token
 gcloud compute scp --compress --recurse \
@@ -43,7 +61,7 @@ gcloud compute scp --compress --recurse \
 
 gcloud compute ssh --ssh-key-file=${KEYNAME} \
       --verbosity debug \
-       ${USERNAME}@${INSTANCE_NAME} -- "GITHUB_USER=${REPO_OWNER} NAME=${REPO_NAME} TAG=${REVISION_ID} " ${COMMAND}
+       ${USERNAME}@${INSTANCE_NAME} -- "GITHUB_USER=${REPO_OWNER} NAME=${REPO_NAME} TAG=${TAG} ${COMMAND}"
 
 gcloud compute scp --compress --recurse \
        ${USERNAME}@${INSTANCE_NAME}:${REMOTE_WORKSPACE}*.log $(pwd) \
